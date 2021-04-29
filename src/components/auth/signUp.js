747ef8472812a817e8b9b7c4f7928b63da573570
Maria-p92 <mariaparradesign@gmail.com>
2021-04-29 13:07:02 +0200

    Main structure

---
+++ src/components/auth/signUp.js
@@ -0,0 +1,166 @@
+import {
+    Avatar, 
+    Button, 
+    CssBaseline, 
+    Grid, 
+    Box, 
+    Typography, 
+    Container, 
+    FormLabel,
+    TextField,
+    FormControlLabel,
+    Radio, 
+    Checkbox,
+    RadioGroup  } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import {useState} from 'react';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://happybites-events.com/">
+        Happy Bites Events
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Signup() {
+  const classes = useStyles();
+  const [formState, setFormState] = useState({
+      firstname:'',
+      lastname:'',
+      email: '',
+      customertype:'',
+      password: ''
+
+  });
+  const {firstname, lastname, email, customertype, password } = formState;
+  const onChange = e => setFormState({...formState, [e.target.name]: e.target.value});
+  const onSubmit = e => {
+      e.preventDefault();
+      console.log('Submitting...')
+  }
+  
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={onSubmit} >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormLabel>What kind of user would you like to be?</FormLabel>
+              <RadioGroup aria-label="customertype" name="customertype" onChange={}>
+                <FormControlLabel  control={<Radio />} label="I am a company and I would like to offer my services" />
+                <FormControlLabel  control={<Radio />} label="I just want to book events and look for inspiration" />
+            </RadioGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+        </Box>
+    </Container>
+  );
+}
