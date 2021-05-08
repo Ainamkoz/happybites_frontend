42ce82043bb0aacbc9c40438814bc71a82ae9a56
linhg91 <mlinh.ng91@gmail.com>
2021-05-08 18:37:36 +0200

    fetched DB and google API

--- src/components/map/mapComponent.js
+++ src/components/map/mapComponent.js
@@ -1,11 +1,33 @@
+import {useEffect, useState} from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import markerCustom from "../assets/img/mapBite.svg";
 import { Link } from "react-router-dom";
 
-const MapComponent = ({ result }) => {
-  console.log("RESULT IN MAPCOMPONENET", result);
+
+
+const MapComponent = () => {
+
+  const [addresses, setAddresses] = useState()
+
+  useEffect(()=>{
+    const getAddresses = async () =>{
+      const res = await fetch ('http://localhost:5000/services/')
+      const data = await res.json()
+      setAddresses(data)
+
+      const resFromGeocoder = await Promise.all(
+        data.map(add => fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${add.address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`))
+      )
+      const dataFromGeocoder = await Promise.all(
+        resFromGeocoder.map(res => res.json())
+      )
+      setAddresses(dataFromGeocoder)
+    }
+    getAddresses()
+  }, [])
+
   const icon = new Icon({
     iconUrl: markerCustom,
     iconSize: [35, 35],
@@ -18,20 +40,19 @@ const MapComponent = ({ result }) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {result && 
-          result.map((markerlocation, result) => (
-          <div key={result.id}>
-            <Marker position={[markerlocation.lat, markerlocation.long]} icon={icon}>
-              <Popup>
-               <Link to={`/company/${markerlocation.id}`}>{markerlocation.restaurantname}, {markerlocation.adress}</Link>
-              </Popup>
-            </Marker>
-          </div>
-        ))}
+      {/*   {
+          addresses && addresses.map((add)=>{
+            return ( 
+              <Marker position={[add.results[0].geometry.lat, add.results[0].geometry.lng]} icon={icon}>
+              </Marker>
+              )
+          })
+        } */}
       </MapContainer>
     </div>
     </>
   );
 };
+
 
 export default MapComponent;
